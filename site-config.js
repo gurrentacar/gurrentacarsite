e3b256c7115d70b9c198dd874e391b2c6dcf0332
gurrentacar <huseyinelald1@icloud.com>
2025-11-29 09:28:19 +0300

    Delete site-config.js

--- site-config.js
+++
@@ -1,7 +0,0 @@
-// Basit site yapılandırma örneği (şimdilik index.html bağımsız çalışır)
-const SITE_CONFIG = {
-  title: "GÜR RENT A CAR | KARAMAN ARAÇ KİRALAMA",
-  phone: "+90 544 970 9712",
-  whatsapp: "+90 544 970 9712",
-  instagram: "@gurrentacar"
-};
